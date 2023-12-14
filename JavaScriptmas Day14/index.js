297794295ff0e58e0e5703c0f83c3467d9feaeca
Sri Om Sharan <124783138+sriom-singh@users.noreply.github.com>
2023-12-14 11:55:55 +0530

    Add files via upload

---
+++ JavaScriptmas Day14/index.js
@@ -0,0 +1,17 @@
+const elf = document.getElementById("elf")
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", duplicateElf)
+
+function duplicateElf(){
+    elf.innerHTML +=`<img class="animate" src="christmas-fairy-tale-character-elf-260nw-2312007105-removebg-preview.png" alt="">`
+  /** Challenge:
+    - Write a function to duplicate the elf when the button is clicked.
+    - See index.css for optional styling challenges.
+  **/
+}
+
+/** Stretch goals:
+  - Write a function to give the elves some tools, or a cup of tea!
+  - Limit the total number of elves to 100.
+**/
